70f4490cffb254be74bec0c969a9e4c6ca875854
Benedikt Mix <benedikt@smirror.io>
2020-07-06 18:29:09 +0200

    initial commit

---
+++ gridsome.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  siteName: "Ain't No Mountain High Enough",
+  plugins: [
+    {
+      use: "@gridsome/source-airtable",
+      options: {
+        // Add these to a .env file
+        // Details on finding these values can be found at:
+        // https://gridsome.org/plugins/@gridsome/source-airtable
+        apiKey: process.env.AIRTABLE_KEY, //required
+        baseId: process.env.AIRTABLE_BASE, //required
+        tables: [
+          {
+            name: "Events",
+            typeName: "Event", //required - needs to match template name
+            select: {}, 
+            links: [], 
+          },
+        ],
+        tableName: "Events", 
+      },
+    },
+  ],
+  templates: {
+    Event: "/events/:id",
+  },
+};
