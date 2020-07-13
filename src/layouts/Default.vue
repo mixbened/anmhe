51d07daeaa88a256225f3579d813c8af13605d37
Benedikt Mix <benedikt@smirror.io>
2020-07-13 13:25:56 +0200

    fully responsive header and footer

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,30 +1,39 @@
 <template>
   <div class="layout">
-    <header class="header">
-
+    <header class="flex p-4 px-8 items-center justify-between">
+      <div>
+        <g-image 
+        alt="Logo ANMHE" 
+        src="https://studyinkenya.co.ke/assets/default/images/ads/institution-logo-placeholder.png"
+        class="w-20 sm:w-32 m:w-40"  
+        />
+      </div>
+      <nav class="sm:mr-20">
+        <div class="flex w-auto">
+          <a class="uppercase p-2 sm:px-6 hover:text-blue-600 hover:underline transition ease-in duration-200 font-semibold" href="#">Link</a>
+          <a class="uppercase p-2 sm:px-6 hover:text-blue-600 hover:underline transition ease-in duration-200 font-semibold"  href="#">Link</a>
+          <a class="uppercase p-2 sm:px-6 hover:text-blue-600 hover:underline transition ease-in duration-200 font-semibold"  href="#">Link</a>
+          <a class="uppercase p-2 sm:px-6 hover:text-blue-600 hover:underline transition ease-in duration-200 font-semibold"  href="#">Link</a>
+        </div>
+      </nav>
     </header>
     <transition name="fade" appear>
       <main>
         <slot />
       </main>
     </transition>
-    <footer class="pt-8">
-      Built with
-      <a
-        href="https://gridsome.org/docs"
-        target="_blank"
-        rel="noopener"
-        class="link--gridsome"
-        >Gridsome</a
-      >
-      by
-      <a
-        href="https://twitter.com/PichuPlayer"
-        target="_blank"
-        rel="noopener"
-        class="link--blue"
-        >Tyler</a
-      >
+    <footer class="flex p-4 px-8 items-center justify-center">
+        <div>
+          <g-image 
+          alt="Logo ANMHE" 
+          src="https://studyinkenya.co.ke/assets/default/images/ads/institution-logo-placeholder.png"
+          class="w-20 m-auto"  
+          />
+          <div class="text-center">
+            <p class="text-xs">Text | text | text | text</p>
+            <p class="text-lg">Text | text | text | text</p>
+          </div>
+        </div>
     </footer>
   </div>
 </template>
@@ -38,5 +47,4 @@ query {
 </static-query>
 
 <style>
-
 </style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,12 @@
 <template>
   <Layout>
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image 
-      alt="Yellow background with grey pencils. Photo by Joanna Kosinska on Unsplash" 
-      src="https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" 
-      width="400" 
-      class="headerImage"
-    />
+    <section>
+      <g-image 
+        alt="Yellow background with grey pencils. Photo by Joanna Kosinska on Unsplash" 
+        src="https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" 
+        class="w-full"
+      />
+    </section>
 
   </Layout>
 </template>
