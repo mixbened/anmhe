316c070ce7f706881ce0402b965499bce47eef9c
Benedikt Mix <benedikt@smirror.io>
2020-10-22 10:04:09 +0200

    embed typeform

--- src/pages/Bergpost.vue
+++ src/pages/Bergpost.vue
@@ -4,8 +4,8 @@
     <section class="mt-20">
       <Intro title="Anfrage, Kontakt & Ähnliches" text="Holladio! Wir sind Ain´t No Mountain, die junge Agentur für Live-Kommunikation im Rheinland. Unser Herz schlägt für echte Erlebnisse: Wir lieben es, Messeauftritte, Produktpräsentationen, Kick-offs, Tagungen, Festivals und Mitarbeiterevents zu konzipieren und umzusetzen. Unser Ohrwurm-Name ist Programm: Kein Berg zu hoch und kein Weg zu weit. Kein Job zu klein und keine Challenge zu groß! Wir entwickeln für jedes Projekt die passenden Tools. On- oder offline, analog oder digital, hybrid oder virtuell. Wie wir das schaffen? Ganz einfach: Mit jeder Menge Erfahrung und unserem Netzwerk aus Kreativen und Projektmanagern. Profis aus Event, Messe, Online, Print und Social Media." />
     </section>
-    <section class="mx-4 sm:mx-20 md:ml-32 px-4">
-      <a href="/was-wir-machen"><button class="bg-tertiary hover:bg-secondary  font-bold py-2 px-4 text-sm">Kontaktanfrage</button></a>
+    <section class="mx-4 sm:mx-20 md:ml-32">
+      <a class="typeform-share button" href="https://form.typeform.com/to/RuigsLYO" data-mode="popup" style="display:inline-block;text-decoration:none;background-color:#FFC31E;color:white;cursor:pointer;font-size:20px;text-align:center;margin:0;padding:10px 22px;border-radius:0px;max-width:100%;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;font-weight:bold;-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale;" target="_blank">Kontaktanfrage</a> 
     </section>
     <!-- <section class="mt-10">
       <div class="mx-4 sm:mx-8 sm:mx-32 md:mx-48 p-4">
