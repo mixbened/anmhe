71b2f5babebccac979c64be3a66fda292d767259
Benedikt Mix <benedikt@smirror.io>
2020-10-31 09:24:19 +0100

    cookies consent

--- src/pages/Bergpost.vue
+++ src/pages/Bergpost.vue
@@ -1,6 +1,6 @@
 <template>
   <Layout>
-    <Header title="Schreib uns."  background="https://images.unsplash.com/photo-1511632765486-a01980e01a18?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"/>
+    <Header title="Schreib uns."  background="https://res.cloudinary.com/mixbened/image/upload/v1603873041/ANM/Titel_WerWir_Sind.png"/>
     <section class="mt-20">
       <Intro title="Anfrage, Kontakt & Ähnliches" text="Holladio! Wir sind Ain´t No Mountain, die junge Agentur für Live-Kommunikation im Rheinland. Unser Herz schlägt für echte Erlebnisse: Wir lieben es, Messeauftritte, Produktpräsentationen, Kick-offs, Tagungen, Festivals und Mitarbeiterevents zu konzipieren und umzusetzen. Unser Ohrwurm-Name ist Programm: Kein Berg zu hoch und kein Weg zu weit. Kein Job zu klein und keine Challenge zu groß! Wir entwickeln für jedes Projekt die passenden Tools. On- oder offline, analog oder digital, hybrid oder virtuell. Wie wir das schaffen? Ganz einfach: Mit jeder Menge Erfahrung und unserem Netzwerk aus Kreativen und Projektmanagern. Profis aus Event, Messe, Online, Print und Social Media." />
     </section>
