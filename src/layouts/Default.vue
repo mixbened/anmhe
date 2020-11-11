a05ecac931946644022e825eedaafc04e4e55331
Benedikt Mix <benedikt@smirror.io>
2020-11-11 16:06:24 +0100

    fix header

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -8,12 +8,12 @@
           class="ml-1 sm:ml-4 w-20"  
           /></g-link>
         </div>
-        <nav class="mr-4 lg:mr-20">
+        <nav class="mr-8 sm:mr-4 lg:mr-20">
           <div class="flex w-auto items-center">
-            <g-link to="/was-wir-machen" class="sm:uppercase p-2 sm:p-2 lg:px-6 hover:text-secondary transition ease-in duration-200 font-semibold text-xs md:text-base">Was Wir Machen</g-link>
-            <g-link to="/wer-wir-sind" class="sm:uppercase p-2 sm:p-2 lg:px-6 hover:text-secondary transition ease-in duration-200 font-semibold text-xs md:text-base">Wer Wir Sind</g-link>
-            <g-link to="/fuer-wen" class="hidden p-2 sm:block sm:uppercase sm:p-2 lg:px-6 hover:text-secondary transition ease-in duration-200 font-semibold text-xs md:text-base">Für Wen</g-link>
-            <g-link to="/was-wir-machen#digital" class="p-2 border border-primary sm:uppercase sm:p-2 lg:px-6 hover:text-secondary transition ease-in duration-200 font-semibold  text-xs md:text-base">Digitale Events</g-link>
+            <g-link to="/was-wir-machen" class="sm:uppercase p-2 p-1 sm:p-2 lg:px-6 hover:text-secondary transition ease-in duration-200 font-semibold text-xs md:text-base">Was Wir Machen</g-link>
+            <g-link to="/wer-wir-sind" class="sm:uppercase p-1 sm:p-2 sm:p-2 lg:px-6 hover:text-secondary transition ease-in duration-200 font-semibold text-xs md:text-base">Wer Wir Sind</g-link>
+            <g-link to="/fuer-wen" class="p-1 sm:p-2 sm:uppercase sm:p-2 lg:px-6 hover:text-secondary transition ease-in duration-200 font-semibold text-xs md:text-base">Für Wen</g-link>
+            <g-link to="/was-wir-machen#digital" class="hidden sm:block p-1 sm:p-2 border border-primary sm:uppercase sm:p-2 lg:px-6 hover:text-secondary transition ease-in duration-200 font-semibold  text-xs md:text-base">Digitale Events</g-link>
           </div>
         </nav>
       </header>
